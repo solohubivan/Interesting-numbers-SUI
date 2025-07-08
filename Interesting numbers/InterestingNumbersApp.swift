0d97556a98104625c2e6b57cb7c583007349b7d5
solohubivan <solohubivan@gmail.com>
2025-07-08 09:02:05 +0300

    Initial Commit

---
+++ Interesting numbers/InterestingNumbersApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterestingNumbersApp.swift
+//  Interesting numbers
+//
+//  Created by Ivan Solohub on 08.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct InterestingNumbersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
